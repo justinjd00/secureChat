a4fa02ee2d209ae978e8b0f14f04e86f32a15ac1
Albano <albigoal@outlook.com>
2024-09-09 22:34:41 +0200

    almost server running 2 ways

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
     server: {
-        port: 3000,
+        port: 3008,
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8069',  // Use port 8062 here
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
     },
+    plugins: [vue()],
 });
